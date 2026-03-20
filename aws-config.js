f982e9edbd97804b4b9963f0981cf7d1a0b21175
Bruh <109279349+Jihoo125@users.noreply.github.com>
2026-03-20 19:15:07 +0900

    Update API base URL in aws-config.js

--- aws-config.js
+++ aws-config.js
@@ -1,9 +1,3 @@
 window.NOTEFLOW_CONFIG = {
-  apiBaseUrl: 'https://abcd1234.execute-api.ap-northeast-2.amazonaws.com',
-  apiKey: '',
-  cognito: {
-    userPoolId: '',
-    userPoolClientId: '',
-    region: 'ap-northeast-2',
-  },
+  apiBaseUrl: 'https://q5tkevwbf0.execute-api.us-east-1.amazonaws.com'
 };
