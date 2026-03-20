4cd2478851b481348599daa26134c80d727d415d
Bruh <109279349+Jihoo125@users.noreply.github.com>
2026-03-20 19:12:53 +0900

    Add AWS configuration for Noteflow

---
+++ aws-config.js
@@ -0,0 +1,9 @@
+window.NOTEFLOW_CONFIG = {
+  apiBaseUrl: 'https://abcd1234.execute-api.ap-northeast-2.amazonaws.com',
+  apiKey: '',
+  cognito: {
+    userPoolId: '',
+    userPoolClientId: '',
+    region: 'ap-northeast-2',
+  },
+};
